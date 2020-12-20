ff9ba8098ebc9d9264e426d68ac6530463d00682
LQ Hoang <longquanhp@gmail.com>
2020-12-19 23:22:03 -0500

    solved day 3 part 2

--- 2020/day3.mjs
+++ 2020/day3.mjs
@@ -1,8 +1,13 @@
 import { input } from "./inputs/day3.mjs";
 
 const TREE = "#";
-const STARTING_COLUMN = 3;
-const STARTING_ROW = 1;
+const SLOPES = [
+  { right: 1, down: 1, numberOfTrees: 0 },
+  { right: 3, down: 1, numberOfTrees: 0 },
+  { right: 5, down: 1, numberOfTrees: 0 },
+  { right: 7, down: 1, numberOfTrees: 0 },
+  { right: 1, down: 2, numberOfTrees: 0 }
+];
 let currentMap = input;
 
 function duplicateMap(input) {
@@ -11,19 +16,26 @@ function duplicateMap(input) {
   });
 }
 
-let numberOfTrees = 0;
-let currentColumn = STARTING_COLUMN;
-let currentRow = STARTING_ROW;
-
-while (currentRow <= currentMap.length - 1) {
-  if (currentColumn >= currentMap[currentRow].length) {
-    duplicateMap(currentMap);
-  }
-  if (currentMap[currentRow].charAt(currentColumn) === TREE) {
-    ++numberOfTrees;
-  }
-  currentColumn += STARTING_COLUMN;
-  currentRow += STARTING_ROW;
+function getTotalNumberOfTrees(slopes) {
+  const numberOfTrees = slopes.map(slope => slope.numberOfTrees);
+  return numberOfTrees.reduce((x, y) => x * y);
 }
 
-console.log("numberOfTrees: ", numberOfTrees);
+let numberOfTrees = 0;
+
+SLOPES.forEach(slope => {
+  let currentColumn = slope.right;
+  let currentRow = slope.down;
+  while (currentRow <= currentMap.length - 1) {
+    if (currentColumn >= currentMap[currentRow].length) {
+      duplicateMap(currentMap);
+    }
+    if (currentMap[currentRow].charAt(currentColumn) === TREE) {
+      slope.numberOfTrees++;
+    }
+    currentColumn += slope.right;
+    currentRow += slope.down;
+  }
+});
+
+console.log("numberOfTrees: ", getTotalNumberOfTrees(SLOPES));
